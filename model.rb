9ecbb9bae6aac2a3080d9264a09bddd54b563de3
alu0100724622 <alu0100724622@ull.edu.es>
2014-11-02 12:13:12 +0000

    creo tabla para estadísticas y relaciones entre tablas

--- model.rb
+++ model.rb
@@ -5,5 +5,19 @@ class ShortenedUrl
   property :url, Text
   property :to, Text
   property :id_usu, Text 
+
+  has n, :visits  
+
 end
 
+class Visit
+  include DataMapper::Resource
+
+  property  :id,          Serial
+  property  :created_at,  DateTime
+  property  :ip,          IPAddress
+  property  :country,     String
+  
+  belongs_to  :shortenedUrl
+
+end
