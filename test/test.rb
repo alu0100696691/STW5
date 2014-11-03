117fa1fbd38e8db40e217b5ad174141f95463c63
alu0100724622 <alu0100724622@ull.edu.es>
2014-11-03 15:58:37 +0000

    test sencillos

--- test/test.rb
+++ test/test.rb
@@ -7,26 +7,13 @@ require 'sinatra'
 require 'data_mapper'
 require_relative '../app'
 
-describe "shortened urls" do
-
-	before :all do
-		@user = "0000"
-		@email = "test@email.es"
-		@url = "http://www.google.es"
-		@urlshort = "google"
-		@country = "SPAIN"
-		@countryCode = "SP"
-		@city = "santa cruz"
-		@latitude = "0000"
-		@longitude = "1111"
-
-		@short_url = Shortenedurl.first_or_create(:uid => @user, :email => @email, :url => @url, :urlshort => @urlshort, :n_visits => 0)
-		@short_url1 = Shortenedurl.first(:urlshort => @urlshort)
-		@short_url.n_visits += 1
-		@short_url.save
-  		@visit = Visit.new(:ip => @ip, :country => @country, :countryCode => @countryCode, :city => @city, :latitude => @latitude, :longitude => @longitude, :shortenedurl => @short_url1, :created_at => Time.now)
-  		@visit.save
+describe "test del home" do
+	
+	it "carga la web el servidor" do
+		get '/'
+		assert last_response.ok?
 	end
+
 
 	it "el titulo de la pagina es TinyClone?" do
 		get '/'
@@ -38,3 +25,4 @@ describe "shortened urls" do
 		get '/'
 		assert_match "<p>© 2014 Sistemas y Tecnologias Web - ULL</p>", last_response.body
 	end
+end
